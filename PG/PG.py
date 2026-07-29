bb376aff20eec1b196fc2f9395c41963015b8a1d
Jycc <2797824480@qq.com>
2026-07-29 16:39:29 +0800

    CartPole-v1配置统一

--- PG/PG.py
+++ PG/PG.py
@@ -1,7 +1,7 @@
 import xuance as xp
 runner = xp.get_runner('PG',
                        'classic_control',  # Choices: claasi_control, box2d, .
-                       'Acrobot-v1',  # Choices: acrobot, cartpole, mountaincar, pendulum, .
-                       "./PG/PG_Acrobot-v1.yaml",  # The path of my_config.yaml file should be correct.
+                       'CartPole-v1',  # Choices: acrobot, cartpole, mountaincar, pendulum, .
+                       "./PG/PG_CartPole-v1.yaml",  # The path of my_config.yaml file should be correct.
                        )
 runner.run(mode='benchmark') 
